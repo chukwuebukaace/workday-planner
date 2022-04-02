8dcf1fea5cbe2bc92d56705846ea4bcf5cde141a
Ebuka Kester <anyadiegwukaycee@icloud.com git config --global user.name chukwuebukaaceclearclear>
2022-04-01 23:43:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Work Day Scheduler Starter Code
+A simple workday application using HTML, CSS and jQuery
+
+## Description
+
+This application is a single-day fixed-interval day planning tool. The code was written using HTML, CSS, Bootstrap CSS framework, JavaScript, jQuery libraries, and font-awesome icon toolkit. The application can be used to record and save tasks to be performed over a standard 9am-5pm workday.
+
+### Features
+
+Current day displayed in application header
+Discrete time block elements for each hour of the working day
+Editable textareas within each time block to record tasks for the specified time period
+Clickable save buttons which will save the text input by the user to localStorage, allowing it to persist across page refreshes
+Color-coding of time blocks relative to the current hour (past, present and future)
+
+#### Installation
+
+Upload index.html, license.txt, and assets folder to the deployed webserver. Assets folder contains JavaScript and CSS files.
+
+The application shown below was deployed using GitHub Pages. The repository can be replicated by via fork, clone, and redeployment with GitHub Pages.
+
+Link to repository: https://github.com/pkriengsiri/workday-planner
+
+##### Usage
+
+Link to deployed instance of the application:
---
+++ assets/scripts/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+    //DOM Variables
+    var dayDisplayEl = $("#currentDay");
+    var containerEl = $(".container");
+  
+    // JS Variables
+    var workdayHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  
+    // Function Definitions
+    function displayDay() {
+      //Write the current day to the display
+      var dayDisplay = moment().format("dddd, MMMM Do");
+      dayDisplayEl.text(dayDisplay);
+    }
+  
+    // Displays the time blocks in the container
+    function displayTimeBlocks() {
+      for (var i = 0; i < workdayHours.length; i++) {
+        //create the row for each time-block
+        var rowEl = $("<row>");
+        rowEl.addClass("row time-block");
+  
+        //create the first column of the time block for the time
+        var hourColEl = $("<col>");
+        hourColEl.addClass("col-1 hour");
+        hourColEl.text(moment().set("hour", workdayHours[i]).format("hA"));
+        rowEl.append(hourColEl);
+  
+        //create the second column of the time block for the text
+        var textColEL = $("<textarea>");
+        textColEL.addClass("col-10 description");
+        textColEL.data("workday-hour", workdayHours[i]);
+        //change the css class for background color based upon the current hour
+        if (moment().hour() > workdayHours[i]) {
+          textColEL.addClass("past");
+        } else if (moment().hour() < workdayHours[i]) {
+          textColEL.addClass("future");
+        } else if (moment().hour() === workdayHours[i]) {
+          textColEL.addClass("present");
+        }
+        //set the text to localStorage saved value if it exists
+        if (localStorage.getItem(workdayHours[i]) !== null) {
+          var savedText = localStorage.getItem(workdayHours[i]);
+          textColEL.text(savedText);
+        }
+        rowEl.append(textColEL);
+  
+        //create the third column for save button
+        var buttonColEl = $("<button>");
+        buttonColEl.addClass("col-1 saveBtn fas fa-save");
+        buttonColEl.hover(
+          function () {
+            $(this).removeClass("fas fa-save");
+            $(this).addClass("far fa-save");
+          },
+          function () {
+            $(this).addClass("fas fa-save");
+          }
+        );
+        rowEl.append(buttonColEl);
+  
+        //append the row
+        containerEl.append(rowEl);
+      }
+    }
+  
+    // Saves the task to localStorage
+    function saveTask(event) {
+      var text = $(this.previousElementSibling).val();
+      var workdayHour = $(this.previousElementSibling).data("workday-hour");
+      localStorage.setItem(workdayHour, text);
+    }
+  
+    // Function Calls
+    displayDay();
+    displayTimeBlocks();
+  
+    // Event Listeners
+    containerEl.on("click", "button", saveTask);
+  });
